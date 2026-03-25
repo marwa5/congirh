ff86cafa3110ff5a7075ea7e5c1d98a9b7f24962
marwa5 <hichrimarwa295@gmail.com>
2026-03-25 01:23:05 +0100

    Create GestioncongesApplication.java file

---
+++ backend/src/main/java/com/example/gestionconges/GestioncongesApplication.java
@@ -0,0 +1,22 @@
+package com.example.gestionconges;
+
+import com.example.gestionconges.entities.Employee;
+import com.example.gestionconges.repositories.EmployeeRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GestioncongesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestioncongesApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(EmployeeRepository employeeRepository) {
+        return args -> {
+        };
+    }
+}
